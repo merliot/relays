f28deacea95a097fa9113bc9922c6b85c42ac0c2
Scott Feldman <sfeldma@gmail.com>
2023-12-03 19:57:46 -0800

    add cfg to runner

--- cmd/main.go
+++ cmd/main.go
@@ -9,8 +9,16 @@ import (
 	"github.com/merliot/relays"
 )
 
+var cfg = runner.Config{
+	Port: "8000",
+	PortPrime: "8001",
+	User: "user",
+	Passwd: "passwd",
+	DialURLs: "ws://127.0.0.1:8001/ws/?ping-period=4",
+}
+
 func main() {
-	relays := relays.New("relays01", "relays", "relays").(*relays.Relays)
+	relays := relays.New("relay01", "relays", "relays").(*relays.Relays)
 	relays.SetDeployParams("target=nano-rp2040&relay1=kitchen+and+bath&relay2=closet&relay3=hallway&relay4=&gpio1=D2&gpio2=D4&gpio3=D5&gpio4=")
-	runner.Run(relays)
+	runner.Run(cfg, relays)
 }
