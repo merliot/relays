a005d70fcebd53122da966b8b8af440867857dae
Scott Feldman <sfeldma@gmail.com>
2023-12-06 20:26:04 -0800

    parse wifi auth

--- cmd/main.go
+++ cmd/main.go
@@ -20,5 +20,6 @@ var cfg = runner.Config{
 func main() {
 	relays := relays.New("relay01", "relays", "relays").(*relays.Relays)
 	relays.SetDeployParams("target=nano-rp2040&relay1=kitchen+and+bath&relay2=closet&relay3=hallway&relay4=&gpio1=D2&gpio2=D4&gpio3=D5&gpio4=")
+	relays.ParseWifiAuth()
 	runner.Run(cfg, relays)
 }
