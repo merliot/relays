e065f2d031cd994ab87e0968aac35271b49069f7
Scott Feldman <sfeldma@gmail.com>
2023-12-27 23:58:28 -0800

    rename to Relays

--- cmd/main.go
+++ cmd/main.go
@@ -11,16 +11,16 @@ import (
 )
 
 var (
-	id = dean.GetEnv("ID", "relay01")
-	name = dean.GetEnv("NAME", "relay01")
+	id           = dean.GetEnv("ID", "relay01")
+	name         = dean.GetEnv("NAME", "Relays")
 	deployParams = dean.GetEnv("DEPLOY_PARAMS", "target=demo&relay1=kitchen&relay2=&relay3=&relay4=&gpio1=DEMO2&gpio2=&gpio3=&gpio4=")
-	port = dean.GetEnv("PORT", "8000")
-	portPrime = dean.GetEnv("PORT_PRIME", "8001")
-	user = dean.GetEnv("USER", "")
-	passwd = dean.GetEnv("PASSWD", "")
-	dialURLs = dean.GetEnv("DIAL_URLS", "ws://127.0.0.1:8001/ws/?ping-period=4")
-	ssids = dean.GetEnv("WIFI_SSIDS", "")
-	passphrases = dean.GetEnv("WIFI_PASSPHRASES", "")
+	port         = dean.GetEnv("PORT", "8000")
+	portPrime    = dean.GetEnv("PORT_PRIME", "8001")
+	user         = dean.GetEnv("USER", "")
+	passwd       = dean.GetEnv("PASSWD", "")
+	dialURLs     = dean.GetEnv("DIAL_URLS", "ws://127.0.0.1:8001/ws/?ping-period=4")
+	ssids        = dean.GetEnv("WIFI_SSIDS", "")
+	passphrases  = dean.GetEnv("WIFI_PASSPHRASES", "")
 )
 
 func main() {
