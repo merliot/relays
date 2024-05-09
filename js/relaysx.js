c9f6f621a8a190d02ce0fd50665453a9d5aa2005
Scott Feldman <sfeldma@gmail.com>
2024-05-09 00:11:07 -0700

    got base working again, moving to prime next

--- js/relaysx.js
+++ js/relaysx.js
@@ -1,5 +1,85 @@
 class DeviceRelays extends DeviceBase {
-	foo() {
-		console.log(`Relays ${this.id} foo() method called`)
+
+	open() {
+		super.open()
+		if (this.state.DeployParams !== "") {
+			this.showRelays()
+		}
+	}
+
+	handle(msg) {
+		switch(msg.Path) {
+		case "click":
+			this.saveClick(msg)
+			break
+		}
+	}
+
+	showRelays() {
+		var havesome = false
+		for (let i = 0; i < 4; i++) {
+			let div = document.getElementById("relay" + i)
+			var relay = this.state.Relays[i]
+			if (relay.Name === "" || relay.Gpio === "") {
+				div.classList.replace("visibleFlex", "hidden")
+			} else {
+				this.setMouse(i)
+				this.setRelayImg(relay, i)
+				this.setGpio(relay, i)
+				this.setRelayName(relay, i)
+				div.classList.replace("hidden", "visibleFlex")
+				havesome = true
+			}
+		}
+		let nodef = document.getElementById("nodef")
+		nodef.classList.replace("visible", "hidden")
+		if (!havesome) {
+			nodef.classList.replace("hidden", "visible")
+		}
+	}
+
+	setMouse(i) {
+		if (this.view === 0) {
+			let div = document.getElementById("relay" + i)
+			div.onclick = () => {
+				if (this.state.Online) {
+					this.relayClick(i)
+				}
+			}
+		}
+	}
+
+	setRelayImg(relay, i) {
+		let image = document.getElementById("relay" + i + "-img")
+		if (relay.State) {
+			image.src = "images/relay-on.png"
+		} else {
+			image.src = "images/relay-off.png"
+		}
+	}
+
+	setGpio(relay, i) {
+		if (this.view === 0) {
+			let gpio = document.getElementById("gpio" + i)
+			gpio.textContent = relay.Gpio
+		}
+	}
+
+	setRelayName(relay, i) {
+		let label = document.getElementById("relay" + i + "-name")
+		label.textContent = relay.Name
+	}
+
+	saveClick(msg) {
+		var relay = this.state.Relays[msg.Relay]
+		relay.State = msg.State
+		this.setRelayImg(relay, msg.Relay)
+	}
+
+	relayClick(index) {
+		var relay = this.state.Relays[index]
+		relay.State = !relay.State
+		this.setRelayImg(relay, index)
+		this.send({Path: "click", Relay: index, State: relay.State})
 	}
 }
